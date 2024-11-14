681ec0b522c2ebda1600630c4d7f5a06601b6fb4
Mikael Lindström <mikael.lindstrom@lindstromsmanagement.se>
2024-11-14 05:58:59 +0100

    Add shared function for show a header or collecting of some integer values

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -22,5 +22,37 @@
             // Make a pause in this program
             Console.ReadKey();
         }
+
+        /// <summary>
+        /// Show a header in console window 
+        /// </summary>
+        /// <param name="headerText">The text in the header</param>
+        static void Header(string headerText)
+        {
+			Console.WriteLine($"*************************");
+			if (headerText.Equals("Menu"))
+				Console.WriteLine($"*\t{headerText}\t\t*");
+			else
+				Console.WriteLine($"*\t{headerText}\t*");
+			Console.WriteLine($"*************************");
+		}
+        /// <summary>
+        /// Collecting of some integer values
+        /// </summary>
+        /// <returns>A array of integer values</returns>
+        static int[] GetSomeInteger()
+        {
+            // Variables for collecting of integer values
+            string numbers;
+            int[] result;
+
+            // Ask the user for some integer values
+            Console.Write("Write some numbers and split them by coma: ");
+            numbers = Console.ReadLine();
+
+            // Convert a string value into a array of integer values then return the result
+            result = Array.ConvertAll(numbers.Split(','), int.Parse);
+            return result;
+        }
     }
 }
