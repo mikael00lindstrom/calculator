ec6cac5b2420fd743cf1824d51e481dbcba7e06e
Mikael Lindström <mikael.lindstrom@lindstromsmanagement.se>
2025-11-17 10:30:42 +0100

    Welcome the user to this console application

--- CalculatorConsoleUI/Program.cs
+++ CalculatorConsoleUI/Program.cs
@@ -1,10 +1,27 @@
 ﻿namespace CalculatorConsoleUI
 {
+    /// <summary>
+    /// The main class of the calculator console application.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// The name of the application.
+        /// </summary>
+        public const string ApplicationName = "The Calculator 2000";
+
+        /// <summary>
+        /// The main entry point of the application.
+        /// </summary>
+        /// <param name="args">A array of command-line arguments</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            // Display a welcome message
+            Console.WriteLine($"Welcome to {ApplicationName}!");
+
+            // Let the user to read the welcome message
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
         }
     }
 }
