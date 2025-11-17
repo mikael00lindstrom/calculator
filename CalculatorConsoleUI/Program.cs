03fbcb385818caf0cb941392b7f5076d0a63dad0
Mikael Lindström <mikael.lindstrom@lindstromsmanagement.se>
2025-11-17 10:39:43 +0100

    Add helper methods to display to header in console or collecting of some integer values

--- CalculatorConsoleUI/Program.cs
+++ CalculatorConsoleUI/Program.cs
@@ -23,5 +23,39 @@
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
         }
+
+        #region Helper methods
+
+        /// <summary>
+        /// Display a header by a specified title in console application
+        /// </summary>
+        /// <param name="title">The specified title for this header</param>
+        static void DispalyAHeader(string title)
+        {
+            Console.WriteLine(new string('*', 50));
+            Console.WriteLine($"* {title}");
+            Console.WriteLine(new string('*', 50));
+        }
+
+        /// <summary>
+        /// Collect some integer from the user
+        /// </summary>
+        /// <returns>A array of some integers</returns>
+        static int[] GetSomeInteger()
+        {
+            // Variables for collecting of some integer
+            string numbers;
+            int[] result;
+
+            // Let the user to enter some integer and split them by coma
+            Console.Write("Enter some number and split them by coma: ");
+            numbers = Console.ReadLine()!;
+
+            // Convert a string value into a array of integer values then return the result
+            result = Array.ConvertAll(numbers.Split(','), int.Parse);
+            return result;
+        }
+
+        #endregion
     }
 }
